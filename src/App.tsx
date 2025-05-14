5f1a3075da3881a6b1e1d113c3582ada60a210c9
inguillen87 <guillen.marce@gmail.com>
2025-05-13 21:50:17 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChatPage from "@/pages/ChatPage";
 
 import Layout from "./components/layout/Layout";
 import Index from "./pages/Index";
