70e0a5288c1ca94e497742de021958d120ff1ad4
inguillen87 <guillen.marce@gmail.com>
2025-05-21 16:50:29 -0300

    Update Iframe.tsx

---
+++ src/pages/Iframe.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatWidget from "@/components/chat/ChatWidget";
+
+const Iframe = () => {
+  return (
+    <div className="w-full h-full">
+      <ChatWidget />
+    </div>
+  );
+};
+
+export default Iframe;
