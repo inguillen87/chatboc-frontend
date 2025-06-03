fadb909fe6f22ea114185a46b47de8f776448fb6
inguillen87 <guillen.marce@gmail.com>
2025-06-03 19:37:59 -0300

    sdf

--- src/pages/Iframe.tsx
+++ src/pages/Iframe.tsx
@@ -1,8 +1,10 @@
+// src/pages/iframe.tsx o /iframe.tsx
+
 import React from "react";
 import ChatWidget from "@/components/chat/ChatWidget";
 
 const Iframe = () => (
-  <div className="w-full h-full">
+  <div className="w-full h-full bg-transparent">
     <ChatWidget />
   </div>
 );
