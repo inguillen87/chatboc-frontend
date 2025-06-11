dbb5a015fe586d8080708e96289a56b00efc3663
inguillen87 <guillen.marce@gmail.com>
2025-06-11 11:05:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // --- SECCIÓN AÑADIDA PARA SOLUCIONAR LA ADVERTENCIA ---
+  build: {
+    rollupOptions: {
+      external: [
+        // Le decimos a Vite: "No te preocupes por el módulo 'path',
+        // sé que es solo para la configuración y no debe ir en el código final."
+        'path'
+      ]
+    }
+  }
 });
