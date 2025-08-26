d5c241bb8a59e80b834deabda5be4a22ada7cdb7
inguillen87 <guillen.marce@gmail.com>
2025-08-26 00:54:17 -0300

    Ensure widget fits viewport and pass sizing params

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       VitePWA({
+        // We manually register the service worker in index.html to avoid
+        // unintentionally registering it inside the embeddable iframe.
+        injectRegister: null,
         registerType: 'autoUpdate',
         includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
         manifest: {
@@ -77,7 +80,7 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         input: {
           main: path.resolve(__dirname, "index.html"),
-          iframe: path.resolve(__dirname, "iframe.html"),
+          iframe: path.resolve(__dirname, "src/pages/iframe.tsx"),
         },
         output: {
           entryFileNames: `assets/[name].js`,
