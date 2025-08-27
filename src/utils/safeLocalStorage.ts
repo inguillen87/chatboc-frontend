c6525e9118ab13d646939960465a93e4063670ae
inguillen87 <guillen.marce@gmail.com>
2025-08-26 23:55:40 -0300

    fix(utils): export named safeLocalStorage

--- src/utils/safeLocalStorage.ts
+++ src/utils/safeLocalStorage.ts
@@ -1,15 +1,21 @@
-import {
-  safeStorage,
-  safeLocalStorageGetItem,
-  safeLocalStorageSetItem,
-  safeLocalStorageRemoveItem,
-  safeLocalStorageClear,
-} from "./safeStorage";
+import { safeStorage } from "./safeStorage";
 
-export const safeLocalStorage = safeStorage;
-export const getLS = safeLocalStorageGetItem;
-export const setLS = safeLocalStorageSetItem;
-export const delLS = safeLocalStorageRemoveItem;
-export const clearLS = safeLocalStorageClear;
+export type SafeLocalStorageLike = {
+  getItem: (k: string) => string | null;
+  setItem: (k: string, v: string) => void;
+  removeItem: (k: string) => void;
+  clear: () => void;
+};
+
+export const safeLocalStorage: SafeLocalStorageLike = {
+  getItem: (k) => safeStorage.getItem(k),
+  setItem: (k, v) => safeStorage.setItem(k, v),
+  removeItem: (k) => safeStorage.removeItem(k),
+  clear: () => safeStorage.clear(),
+};
+
+export const getLS = (k: string) => safeLocalStorage.getItem(k);
+export const setLS = (k: string, v: string) => safeLocalStorage.setItem(k, v);
+export const delLS = (k: string) => safeLocalStorage.removeItem(k);
 
 export default safeLocalStorage;
